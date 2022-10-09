b399a6a589e01bb514d2bd5c7e8dbc3cb079efab
imars1014 <a25896365@gmail.com>
2022-10-09 15:55:07 +0800

    swap_finish

--- swap.c
+++ swap.c
@@ -1,3 +1,19 @@
 # include <stdio.h>
 # include <stdlib.h>
  int a=3,b=5;
+ int *addrA=&a;
+ int *addrB=&b;
+void swap();
+
+ int main (){
+    if (a<b){
+        swap(addrA,addrB);
+        printf("a=%d\nb=%d\n",a,b);
+    }
+    return 0;
+ }
+void swap(int*a,int*b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
